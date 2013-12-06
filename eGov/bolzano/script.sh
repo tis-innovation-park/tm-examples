1500a6f050d4d3e24f596ef05618db1b637adc47
Henrik Sandklef <hesa@sandklef.com>
2013-12-06 11:51:26 +0100

    Added error checks Possible to pass client ip parameter

--- eGov/bolzano/script.sh
+++ eGov/bolzano/script.sh
@@ -1,4 +1,32 @@
+#!/bin/sh
+
+CLIENT_IP=localhost
+if [ "$1" = "--client" ]
+then
+    CLIENT_IP=$2
+fi
+
+exit_on_failure() 
+{
+    RET_VAL=$1
+    COMMENT=$2
+
+    if [ "$RET_VAL" = "0" ]
+    then
+	echo "ERROR: $RET_VAL"
+	echo $COMMENT
+	exit $RET_VAL
+    fi
+}
+
 tm-vmm --client-copy-file selenium-server-standalone-2.37.0.jar $CLIENT_NAME:~/selenium-server-standalone-2.37.0.jar
+exit_on_failure $? "Copying selenium-server jar to $CLIENT_NAME"
+
 tm-vmm --client-exec $CLIENT_NAME "killall java; DISPLAY=:0 nohup java -jar selenium-server-standalone-2.37.0.jar > out 2> err < /dev/null &"
+exit_on_failure $? "Start Selenium on $CLIENT_NAME"
+
 sleep 15
-ant -Dtm.seleniumDriver=RemoteWebDriver -Dtm.seleniumUrl=http://localhost:4444/wd/hub
+
+ant -Dtm.seleniumDriver=RemoteWebDriver -Dtm.seleniumUrl=http://${CLIENT_IP}:4444/wd/hub
+exit_on_failure $? "Executing ant build file"
+
