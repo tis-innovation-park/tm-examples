33d8196a17b0f90f516117604032b689aa0e7bab
Jon Kristensen <info@jonkri.com>
2013-12-10 23:03:25 +0100

    eGov/bolzano/script.sh: Make script work with Android

--- eGov/bolzano/script.sh
+++ eGov/bolzano/script.sh
@@ -22,35 +22,54 @@ echo -e "\t<client-start>$CLIENT_STARTED</client-start>" >> $XML_FILE
 
 if [ "$CLIENT_STARTED" = "0" ]; then
     echo -e "\t<client-up>$CLIENT_UP</client-up>" >> $XML_FILE
-    echo -e "\t<client-ssh>$CLIENT_SSH_UP</client-ssh>" >> $XML_FILE
-    if [ "$CLIENT_SSH_UP" = "0" ]; then
+    if [ "$CLIENT_TYPE" = "VirtualBox" ]; then
+	echo -e "\t<client-ssh>$CLIENT_SSH_UP</client-ssh>" >> $XML_FILE
+	if [ "$CLIENT_SSH_UP" = "0" ]; then
+            echo -e "\t<test>" >> $XML_FILE
+            echo -e "\t\t<name>Bolzano</name>" >> $XML_FILE
+
+            tm-vmm --client-copy-file selenium-server-standalone-2.37.0.jar $CLIENT_NAME:~/selenium-server-standalone-2.37.0.jar
+            exit_on_failure $? "Copying selenium-server jar to $CLIENT_NAME"
+
+            tm-vmm --client-exec $CLIENT_NAME "killall java; DISPLAY=:0 nohup java -jar selenium-server-standalone-2.37.0.jar > out 2> err < /dev/null &"
+            exit_on_failure $? "Start Selenium on $CLIENT_NAME"
+
+            sleep 15
+            exit_on_failure $? "Computer is not tired today"
+
+            rm -R test_results/$CLIENT_NAME-$DATE 2> /dev/null
+            ant -Dtm.seleniumDriver=RemoteWebDriver -Dtm.seleniumUrl=http://localhost:4444/wd/hub -Dtm.testngOutput=test_results/$CLIENT_NAME-$DATE
+            exit_on_failure $? "Executing ant build file"
+	fi
+    elif [ "$CLIENT_TYPE" = "Android" ]; then
+	echo -e "\t<client-ssh>-1</client-ssh>" >> $XML_FILE
+
         echo -e "\t<test>" >> $XML_FILE
         echo -e "\t\t<name>Bolzano</name>" >> $XML_FILE
 
-        tm-vmm --client-copy-file selenium-server-standalone-2.37.0.jar $CLIENT_NAME:~/selenium-server-standalone-2.37.0.jar
-        exit_on_failure $? "Copying selenium-server jar to $CLIENT_NAME"
+	tm-vmm --install-app $CLIENT_NAME android-server-2.21.0.apk
 
-        tm-vmm --client-exec $CLIENT_NAME "killall java; DISPLAY=:0 nohup java -jar selenium-server-standalone-2.37.0.jar > out 2> err < /dev/null &"
-        exit_on_failure $? "Start Selenium on $CLIENT_NAME"
+	tm-vmm --client-exec $CLIENT_NAME "am start -n org.openqa.selenium.android.app/.MainActivity"
 
-        sleep 15
-        exit_on_failure $? "Computer is not tired today"
+	adb -s $ANDROID_DEVICE forward tcp:8080 tcp:8080
 
-        ant -Dtm.seleniumDriver=RemoteWebDriver -Dtm.seleniumUrl=http://localhost:4444/wd/hub -Dtm.testngOutput=test_results/$CLIENT_NAME-$DATE
+        ant -Dtm.seleniumDriver=AndroidDriver -Dtm.testngOutput=test_results/$CLIENT_NAME-$DATE
         exit_on_failure $? "Executing ant build file"
-
-        if [ -e "test_results/$CLIENT_NAME-$DATE/testng-failed.xml" ]; then
-            echo -e "\t\t<result>1</result>" >> $XML_FILE
-        else
-            echo -e "\t\t<result>0</result>" >> $XML_FILE
-        fi
-
-        echo -e "\t\t<logfile>`readlink -f test_results/$CLIENT_NAME-$DATE/index.html`</logfile>" >> $XML_FILE
-        echo -e "\t</test>" >> $XML_FILE
-
-        echo -e "\t<client-stop>$?</client-stop>" >> $XML_FILE
-        echo -e "\t<stop-time>`date "+%Y-%m-%d %H:%I:%S"`</stop-time>" >> $XML_FILE
+    else
+	exit_on_failure 1 "Unsupported client type: $CLIENT_TYPE"
     fi
+
+    if [ -e "test_results/$CLIENT_NAME-$DATE/testng-failed.xml" ]; then
+	echo -e "\t\t<result>1</result>" >> $XML_FILE
+    else
+	echo -e "\t\t<result>0</result>" >> $XML_FILE
+    fi
+
+    echo -e "\t\t<logfile>`readlink -f test_results/$CLIENT_NAME-$DATE/index.html`</logfile>" >> $XML_FILE
+    echo -e "\t</test>" >> $XML_FILE
+
+    echo -e "\t<client-stop>$?</client-stop>" >> $XML_FILE
+    echo -e "\t<stop-time>`date "+%Y-%m-%d %H:%I:%S"`</stop-time>" >> $XML_FILE
 
     tm-vmm --stop-client $CLIENT_NAME
 
